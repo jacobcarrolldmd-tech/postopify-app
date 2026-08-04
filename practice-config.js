3a6f4865cf483afa835ce1fb68a7bf19eefa6d21
Ybeestva <268453439+jacobcarrolldmd-tech@users.noreply.github.com>
2026-08-03 21:07:58 -0700

    Update practice-config.js

--- practice-config.js
+++ practice-config.js
@@ -28,7 +28,7 @@ function getPracticeId() {
 }
 
 // ---- 2. Fetch that practice's config from Apps Script ----
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyW5sgcGa5aK9LWh_5bDVAq8aenM1Y4G2Cf5CmnPXb3TsVoDo8FQWLhrT-SgME0r5eh/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbza80m6HfLTupH_CPHJ1QHv-iygozQgERmu3VtmY4GRA0fjc522RKtVOnWvqyqI-AVy/exec";
 
 async function loadPracticeConfig() {
   const practiceId = getPracticeId();
